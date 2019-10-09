1c046d7225f646d0ff062e7a27abe3d937aca889
Lee Guo En <guoen472@hotmail.com>
2019-10-10 02:37:58 +0800

    discord scheduler

--- discord_bot_helper.js
+++ discord_bot_helper.js
@@ -1,8 +1,16 @@
 var auth = require('./auth.json');
 const Discord = require('discord.js');
 var atob = require('atob');
+var schedule = require('node-schedule');
 
 const client = new Discord.Client();
+
+var scheduler = () => {
+    return schedule.scheduleJob('40 * * * *', function () {
+        client.channels.get("600894246899286047").send('Scheduler Triggered at minute 40!!');
+    });
+}
+
 
 module.exports = (res) => {
 
@@ -12,7 +20,7 @@ module.exports = (res) => {
         } else {
             console.log(`Logged in as ${client.user.tag}!`);
         }
-
+        var sche = scheduler();
     });
 
     client.on('message', msg => {
