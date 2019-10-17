d8649ac455f0f566ac4c660f0548513604ab676e
Solomon Anthony Lee <LeeS@SOUTH.SCWCSU.NHS.UK>
2019-10-17 15:32:35 +0100

    ready for sql connections

---
+++ db_helper.js
@@ -0,0 +1,35 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('../discord_db/discord_bot_siege_db');
+ 
+let sql = `SELECT * FROM Configuration`;
+ 
+// first row only
+db.get(sql, [], (err, row) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  return row
+    ? console.log(row.id, row.name)
+    : console.log(`no config found!`);
+ 
+});
+
+db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row.name);
+    });
+  });
+
+
+// close the database connection
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
