4a593ac6eccc4e719c1c1a7d7323bb5a92a00ce0
Solomon Anthony Lee <LeeS@SOUTH.SCWCSU.NHS.UK>
2019-10-17 17:23:58 +0100

    sqllite3

--- db_tester.js
+++ db_tester.js
@@ -1,7 +1,9 @@
 const db = require('./db_helper');
 
+console.log(db);
+
 var db_s = db();
-console.log(db_s, db);
+console.log(db_s);
 
 db_s.configuration_all_rows.then((result) => {
     console.log('db_tester : ', result)
