ac324bb6baad01712ae6ed182203ec907a68109e
Solomon Anthony Lee <LeeS@SOUTH.SCWCSU.NHS.UK>
2019-10-17 17:27:34 +0100

    sqllite3

--- db_tester.js
+++ db_tester.js
@@ -3,9 +3,9 @@ var discord = require('./discord_bot_helper');
 console.log(discord);
 console.log(db);
 
-var db_s = db();
-console.log(db_s);
+// var db_s = db();
+// console.log(db_s);
 
-db_s.configuration_all_rows.then((result) => {
-    console.log('db_tester : ', result)
-})
+// db_s.configuration_all_rows.then((result) => {
+//     console.log('db_tester : ', result)
+// })
