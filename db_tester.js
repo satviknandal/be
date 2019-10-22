7b8170789298ee3711a4a1144133e5aa6a56a77f
Solomon Anthony Lee <LeeS@SOUTH.SCWCSU.NHS.UK>
2019-10-22 12:35:49 +0100

    updated db functs

--- db_tester.js
+++ db_tester.js
@@ -8,9 +8,10 @@ let client = new Discord.Client();
 var db_helper = db();
 
 db_helper.guild_all_rows().then((guildRows) => {
-    guildRows.forEach(async (gRow) => {
+
+    guildRows.forEach(async (gRow, g_index) => {
         let eventRows = await db_helper.event_all_rows(gRow.ID);
-        eventRows.forEach(async (eRow) => {
+        eventRows.forEach(async (eRow, e_index) => {
             let settings = {
                 ...eRow,
                 guildID: gRow.ID,
@@ -21,9 +22,16 @@ db_helper.guild_all_rows().then((guildRows) => {
 
             let rtn_client = await discord(settings);
             client = rtn_client;
+
+            //get last execution
+            if ((g_index == (guildRows.length - 1)) && (e_index == (eventRows.length - 1))) {
+                console.log('Last Execution : ', g_index, e_index);
+                client.login(atob(auth.token));
+            }
         })
     })
 
-    client.login(atob(auth.token));
-    console.log('???');
+   
+    
 })
+
