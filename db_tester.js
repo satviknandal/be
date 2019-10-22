42be272a109b799a704007f6656dc2029228d04e
Solomon Anthony Lee <LeeS@SOUTH.SCWCSU.NHS.UK>
2019-10-22 12:01:25 +0100

    updated db functs

--- db_tester.js
+++ db_tester.js
@@ -1,28 +1,29 @@
 const db = require('./db_helper');
 const discord = require('./discord_bot_integrated_helper');
 const Discord = require('discord.js');
+var auth = require('./auth.json');
 let client = new Discord.Client();
 
 var db_helper = db();
 
 db_helper.guild_all_rows().then((guildRows) => {
-    guildRows.forEach((gRow) => {
-        db_helper.event_all_rows(gRow.ID).then((eventRows) => {
-            console.log('eventRows', eventRows);
-            eventRows.forEach(async (eRow) => {
-                let settings = {
-                    ...eRow,
-                    guildID: gRow.ID,
-                    guild_Discord_ID: gRow.Discord_ID,
-                    Developer_ID: gRow.Developer_ID,
-                    dis_client: client
-                }
-                if (gRow.ID == 3) {
-                    let client = await discord(settings);
-                    console.log(client);
-                }
+    guildRows.forEach(async (gRow) => {
+        let eventRows = await db_helper.event_all_rows(gRow.ID);
 
-            })
+        console.log('eventRows', eventRows);
+        eventRows.forEach(async (eRow) => {
+            let settings = {
+                ...eRow,
+                guildID: gRow.ID,
+                guild_Discord_ID: gRow.Discord_ID,
+                Developer_ID: gRow.Developer_ID,
+                dis_client: client
+            }
+
+            let rtn_client = await discord(settings);
+            client = rtn_client;
         })
     })
+
+    client.login(atob(auth.token));
 })
